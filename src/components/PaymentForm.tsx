37e83d1b2619d99778c464b1ab72af9961952a2a
liyang <xxbarca@163.com>
2026-04-07 20:30:11 +0800

    购物车设计

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,7 +1,77 @@
 'use client'
+import {useForm, SubmitHandler} from "react-hook-form";
+import {PaymentFormInputs, paymentFormSchema} from "@/types";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { ShoppingCart} from "lucide-react";
+import {useRouter} from "next/navigation";
+import Image from "next/image";
 
 const PaymentForm = () => {
-  return <></>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PaymentFormInputs>({
+    resolver: zodResolver(paymentFormSchema)
+  })
+  const router = useRouter();
+  const handlePaymentForm: SubmitHandler<PaymentFormInputs> = (data: PaymentFormInputs) => {
+
+  }
+  return <form className={'flex flex-col gap-4'} onSubmit={handleSubmit(handlePaymentForm)}>
+    <div className={'flex flex-col gap-1'}>
+      <label htmlFor={'cardHolder'} className={'text-xs text-gray-500 font-medium'}>cardHolder</label>
+      <input
+        className={'border-b border-gray-200 py-2 outline-none text-sm'}
+        type={'text'}
+        id={'cardHolder'}
+        placeholder={'please input cardHolder'} {...register("cardHolder")}
+      />
+      {errors.cardHolder && <p className="text-xs text-red-500">{errors.cardHolder.message}</p>}
+    </div>
+    <div className={'flex flex-col gap-1'}>
+      <label htmlFor={'cardNumber'} className={'text-xs text-gray-500 font-medium'}>cardNumber</label>
+      <input
+        className={'border-b border-gray-200 py-2 outline-none text-sm'}
+        type={'text'}
+        id={'cardNumber'}
+        placeholder={'please input cardNumber'} {...register("cardNumber")}
+      />
+      {errors.cardNumber && <p className="text-xs text-red-500">{errors.cardNumber.message}</p>}
+    </div>
+    <div className={'flex flex-col gap-1'}>
+      <label htmlFor={'expirationDate'} className={'text-xs text-gray-500 font-medium'}>expirationDate</label>
+      <input
+        className={'border-b border-gray-200 py-2 outline-none text-sm'}
+        type={'text'}
+        id={'expirationDate'}
+        placeholder={'please input expirationDate'} {...register("expirationDate")}
+      />
+      {errors.expirationDate && <p className="text-xs text-red-500">{errors.expirationDate.message}</p>}
+    </div>
+    <div className={'flex flex-col gap-1'}>
+      <label htmlFor={'cvv'} className={'text-xs text-gray-500 font-medium'}>CVV</label>
+      <input
+        className={'border-b border-gray-200 py-2 outline-none text-sm'}
+        type={'text'}
+        id={'cvv'}
+        placeholder={'please input cvv'} {...register("cvv")}
+      />
+      {errors.cvv && <p className="text-xs text-red-500">{errors.cvv.message}</p>}
+    </div>
+    <div className={'flex items-center gap-2 mt-4'}>
+      <Image src={'/klarna.png'} alt={''} width={50} height={25} className={'rounded-md'}/>
+      <Image src={'/cards.png'} alt={''} width={50} height={25} className={'rounded-md'}/>
+      <Image src={'/stripe.png'} alt={''} width={50} height={25} className={'rounded-md'}/>
+    </div>
+    <button
+      type={'submit'}
+      className={'w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2'}
+    >
+      Checkout
+      <ShoppingCart className={'w-3 h-3'}/>
+    </button>
+  </form>
 }
 
 export default PaymentForm
--- src/types.ts
+++ src/types.ts
@@ -33,3 +33,12 @@ export const shippingFormSchema = z.object({
 })
 
 export type ShippingFormInputs = z.infer<typeof shippingFormSchema>
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "CardHolder is required"),
+  cardNumber: z.string().min(16, "CardNumber is required").max(16, "CardNumber is required"),
+  expirationDate: z.string().regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Expiration date must be MM/YY format"),
+  cvv: z.string().min(3, "CVV is required").max(3, "CVV is required"),
+})
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>
